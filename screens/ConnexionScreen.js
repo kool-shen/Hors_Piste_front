b5ca1eccd766221bf565647470035a50d9ec2440
chenanef <ychenanef@gmail.com>
2022-12-14 11:27:47 +0100

    usewindowsdimension

--- screens/ConnexionScreen.js
+++ screens/ConnexionScreen.js
@@ -5,6 +5,7 @@ import {
   Text,
   TouchableOpacity,
   Image,
+  useWindowDimensions,
 } from "react-native";
 import { useState } from "react";
 import Logo from "../assets/Logo.png";
@@ -18,6 +19,7 @@ export default function ConnexionScreen({ navigation }) {
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { styles } = useStyle();
 
   const handleConnect = async () => {
     console.log(user);
@@ -51,141 +53,158 @@ export default function ConnexionScreen({ navigation }) {
       style={styles.mainContainer}
       behavior={Platform.OS === "ios" ? "padding" : "height"}
     >
-      <View style={styles.mainContainer}>
-        <View style={styles.container}></View>
-        <View style={styles.background}>
-          <View style={styles.subBackground}>
-            <Image source={Logo} style={styles.logo} resizeMode="contain" />
-            <View style={styles.containerSignin}>
-              <View style={styles.inputContainer}>
-                <Text style={styles.inputText}>Email</Text>
-                <TextInput
-                  style={styles.input}
-                  value={email}
-                  onChangeText={(value) => setEmail(value)}
-                />
-              </View>
-              <View style={styles.inputContainer}>
-                <Text style={styles.inputText}>Mot de passe</Text>
-                <TextInput
-                  style={styles.input}
-                  value={password}
-                  onChangeText={(value) => setPassword(value)}
-                />
-              </View>
-              <TouchableOpacity
-                style={styles.validateButton}
-                onPress={() => handleConnect()}
-              >
-                <Text style={styles.validate}>Valider</Text>
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <Text style={styles.forgot}>Mot de passe oublié ?</Text>
-              </TouchableOpacity>
+      <View style={styles.firstContainer}>
+        <Text>{"\n"}</Text>
+        <View style={styles.mainContainer}>
+          <View style={styles.container}></View>
+          <View style={styles.background}>
+            <View style={styles.subBackground}>
+              <Image source={Logo} style={styles.logo} resizeMode="contain" />
+              <View style={styles.containerSignin}>
+                <View style={styles.inputContainer}>
+                  <Text style={styles.inputText}>Email</Text>
+                  <TextInput
+                    style={styles.input}
+                    value={email}
+                    onChangeText={(value) => setEmail(value)}
+                  />
+                </View>
+                <View style={styles.inputContainer}>
+                  <Text style={styles.inputText}>Mot de passe</Text>
+                  <TextInput
+                    style={styles.input}
+                    value={password}
+                    onChangeText={(value) => setPassword(value)}
+                  />
+                </View>
+                <TouchableOpacity
+                  style={styles.validateButton}
+                  onPress={() => handleConnect()}
+                >
+                  <Text style={styles.validate}>Valider</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Text style={styles.forgot}>Mot de passe oublié ?</Text>
+                </TouchableOpacity>
 
-              <Text style={styles.notYet}>Pas encore de compte ?</Text>
-              <TouchableOpacity style={styles.createButton}>
-                <Text style={styles.createText}>Créer un compte</Text>
-              </TouchableOpacity>
+                <Text style={styles.notYet}>Pas encore de compte ?</Text>
+                <TouchableOpacity style={styles.createButton}>
+                  <Text style={styles.createText}>Créer un compte</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         </View>
+        <Text>Heigth: {styles.firstContainer.height.toString()}</Text>
+        <Text>Width: {styles.firstContainer.width.toString()}</Text>
       </View>
     </KeyboardAwareScrollView>
   );
 }
-const styles = StyleSheet.create({
-  mainContainer: {
-    backgroundColor: "#F8DFBD",
-    height: "100%",
-    width: "100%",
-    flex: 1,
-    zIndex: -1,
-  },
+const useStyle = () => {
+  const dimensions = useWindowDimensions();
+  console.log("Logging dimensions", dimensions);
 
-  background: {
-    backgroundColor: "#A5D8E6",
-    transform: [
-      { rotate: "-35deg" },
-      { translateX: -100 },
-      { translateY: -50 },
-    ],
-    height: "100%",
-    width: 600,
-    flex: 1,
-    alignItems: "center",
-  },
-  subBackground: {
-    transform: [{ rotate: "35deg" }, { translateX: 9 }, { translateY: -16 }],
-    height: "100%",
-    width: 300,
-    display: "flex",
-    alignItems: "center",
-  },
-  containerSignin: {
-    display: "flex",
-    alignItems: "center",
-    height: 350,
-    justifyContent: "space-between",
-  },
+  const styles = StyleSheet.create({
+    mainContainer: {
+      backgroundColor: "#F8DFBD",
+      height: "100%",
+      width: "100%",
+      flex: 1,
+      zIndex: -1,
+    },
 
-  inputContainer: {
-    height: 70,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  input: {
-    backgroundColor: "white",
-    height: 40,
-    width: 250,
-    borderColor: "gray",
-    borderWidth: 1,
-    placeholderTextColor: "gray",
-  },
-  createButton: {
-    paddingHorizontal: 30,
-    paddingVertical: 10,
-    borderRadius: 10,
-    backgroundColor: "#143143",
-  },
-  createText: {
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 20,
-  },
-  inputText: {
-    backgroundColor: "#143143",
-    maxWidth: 150,
-    textAlign: "center",
-    fontSize: 15,
-    borderRadius: 5,
-    color: "white",
-    paddingHorizontal: 10,
-  },
-  forgot: {
-    fontWeight: "bold",
-    color: "red",
-  },
+    background: {
+      backgroundColor: "#A5D8E6",
+      transform: [
+        { rotate: "-35deg" },
+        { translateX: -100 },
+        { translateY: -50 },
+      ],
+      height: "100%",
+      width: 600,
+      flex: 1,
+      alignItems: "center",
+    },
+    subBackground: {
+      transform: [{ rotate: "35deg" }, { translateX: 9 }, { translateY: -16 }],
+      height: "100%",
+      width: 300,
+      display: "flex",
+      alignItems: "center",
+    },
+    containerSignin: {
+      display: "flex",
+      alignItems: "center",
+      height: 350,
+      justifyContent: "space-between",
+    },
 
-  notYet: {
-    color: "white",
-    paddingTop: 20,
-  },
+    inputContainer: {
+      height: 70,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+    },
+    input: {
+      backgroundColor: "white",
+      height: 40,
+      width: 250,
+      borderColor: "gray",
+      borderWidth: 1,
+      placeholderTextColor: "gray",
+    },
+    createButton: {
+      paddingHorizontal: 30,
+      paddingVertical: 10,
+      borderRadius: 10,
+      backgroundColor: "#143143",
+    },
+    createText: {
+      color: "white",
+      fontWeight: "bold",
+      fontSize: 20,
+    },
+    inputText: {
+      backgroundColor: "#143143",
+      maxWidth: 150,
+      textAlign: "center",
+      fontSize: 15,
+      borderRadius: 5,
+      color: "white",
+      paddingHorizontal: 10,
+    },
+    forgot: {
+      fontWeight: "bold",
+      color: "red",
+    },
 
-  validateButton: {
-    backgroundColor: "green",
-    paddingHorizontal: 40,
-    borderRadius: 10,
-  },
-  validate: {
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 25,
-  },
-  logo: {
-    width: "100%",
-    height: 300,
-    marginLeft: 90,
-  },
-});
+    notYet: {
+      color: "white",
+      paddingTop: 20,
+    },
+
+    validateButton: {
+      backgroundColor: "green",
+      paddingHorizontal: 40,
+      borderRadius: 10,
+    },
+    validate: {
+      color: "white",
+      fontWeight: "bold",
+      fontSize: 25,
+    },
+    logo: {
+      width: "100%",
+      height: 300,
+      marginLeft: 90,
+    },
+    firstContainer: {
+      height: dimensions.height,
+      width: dimensions.width,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  });
+  return { styles };
+};
