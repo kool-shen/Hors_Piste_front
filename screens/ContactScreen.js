b31150e4a74f13d4fc3e9f1f6986309abe73801c
chenanef <ychenanef@gmail.com>
2022-12-16 15:42:29 +0100

    noupdate

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,49 +1,54 @@
-import { View, Text,StyleSheet,Image,useWindowDimensions,} from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  useWindowDimensions,
+} from "react-native";
+import React from "react";
 
 const ContactScreen = () => {
   const styles = makeStyles();
   return (
-  
-    <View  style={ styles.container }>
-       <Image source={require('../assets/contact.png')} style={ styles.container } />
-       <View style={styles.pageTitleContainer}>
-            <Text style={styles.pageTitle}>Mes Contacts</Text>
-          </View>
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/contact.png")}
+        style={styles.container}
+      />
+      <View style={styles.pageTitleContainer}>
+        <Text style={styles.pageTitle}>Mes Contacts</Text>
+      </View>
 
-          <View style={styles.firstContainer}>
-            <View>
-              <Text style={styles.textContainer}>Référent Hors Piste</Text>
-            </View>
+      <View style={styles.firstContainer}>
+        <View>
+          <Text style={styles.textContainer}>Référent Hors Piste</Text>
+        </View>
 
-            <Text style={styles.infoContainer}>
-              Name:Benjamin Tagawa{"\n"}N°:06 16 66 65 43{"\n"}
-              Mail:btag@horspistes.com
-            </Text>
-          </View>
+        <Text style={styles.infoContainer}>
+          Name:Benjamin Tagawa{"\n"}N°:06 16 66 65 43{"\n"}
+          Mail:btag@horspistes.com
+        </Text>
+      </View>
 
-          <View style={styles.secondContainer}>
-            <View>
-              <Text style={styles.textContainer}>Référent HAS</Text>
-            </View>
-            <Text style={styles.infoContainer}>
-              Name:Julien Hopper{"\n"}N°:06 16 66 65 43{"\n"}
-              Mail:leh-has@gmail.com
-            </Text>
-          </View>
+      <View style={styles.secondContainer}>
+        <View>
+          <Text style={styles.textContainer}>Référent HAS</Text>
+        </View>
+        <Text style={styles.infoContainer}>
+          Name:Julien Hopper{"\n"}N°:06 16 66 65 43{"\n"}
+          Mail:leh-has@gmail.com
+        </Text>
+      </View>
     </View>
-    
-  )
-}
+  );
+};
 const makeStyles = () => {
   const { fontScale } = useWindowDimensions();
-return(
-  StyleSheet.create({
+  return StyleSheet.create({
     container: {
       flex: 1,
-      width:"100%",
-      height:"100%",
-      
+      width: "100%",
+      height: "100%",
     },
     pageTitle: {
       color: "white",
@@ -63,24 +68,19 @@ return(
       justifyContent: "space-between",
       padding: 10,
     },
-    firstContainer:{
-backgroundColor:"red",
-
+    firstContainer: {
+      backgroundColor: "red",
     },
-    textContainer:{
-      fontSize:30,
-
+    textContainer: {
+      fontSize: 30,
     },
-    infoContainer:{ 
-fontSize:15,
+    infoContainer: {
+      fontSize: 15,
     },
-    secondContainer:{
-backgroundColor:"violet"
-    }
-  })
- )}
+    secondContainer: {
+      backgroundColor: "violet",
+    },
+  });
+};
 
- 
-
-export default ContactScreen
-
+export default ContactScreen;
