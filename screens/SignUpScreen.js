d3c6558b43f4da84dc3b7c8c67a3a9dea4e07f5a
Leo <ferteleo@gmail.com>
2022-12-19 10:17:13 +0100

    monday_commit

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,11 +1,7 @@
 import { useState } from "react";
-import {
-  StyleSheet,
-  useWindowDimensions,
-  View
-} from "react-native";
+import { StyleSheet, useWindowDimensions, View } from "react-native";
 
-import SignUpStep0 from "../component/signUpSteps/SignUpStep1";
+import SignUpStep0 from "../component/signUpSteps/SignUpStep0";
 import SignUpStep1 from "../component/signUpSteps/SignUpStep1";
 import SignUpStep2 from "../component/signUpSteps/SignUpStep2";
 import SignUpStep3 from "../component/signUpSteps/SignUpStep3";
@@ -24,7 +20,9 @@ export default function SignUpScreen({ navigation }) {
   let activeStep;
 
   if (stepValue === 0) {
-    activeStep = <SignUpStep0 navigation={navigation} nextStep={() => nextStep()} />
+    activeStep = (
+      <SignUpStep0 navigation={navigation} nextStep={() => nextStep()} />
+    );
   } else if (stepValue === 1) {
     activeStep = <SignUpStep1 nextStep={() => nextStep()} />;
   } else if (stepValue === 2) {
@@ -40,9 +38,9 @@ export default function SignUpScreen({ navigation }) {
   } else if (stepValue === 7) {
     activeStep = <SignUpStep7 nextStep={() => nextStep()} />;
   } else {
-   navigation.navigate('TabNavigator')
-   return
-  } 
+    navigation.navigate("TabNavigator");
+    return;
+  }
 
   return <View style={styles.mainContainer}>{activeStep}</View>;
 }
@@ -55,19 +53,19 @@ const makeStyles = () => {
       height: "100%",
       width: "100%",
       flex: 1,
-      zIndex: -1
+      zIndex: -1,
     },
     background: {
       backgroundColor: "#A5D8E6",
       transform: [
         { rotate: "-35deg" },
         { translateX: -100 },
-        { translateY: -50 }
+        { translateY: -50 },
       ],
       height: "100%",
       width: 600,
       flex: 1,
-      alignItems: "center"
+      alignItems: "center",
     },
     subBackground: {
       transform: [{ rotate: "35deg" }, { translateX: 9 }, { translateY: -16 }],
@@ -77,13 +75,13 @@ const makeStyles = () => {
       alignItems: "center",
       justifyContent: "space-around",
       paddingTop: 130,
-      paddingBottom: 20
+      paddingBottom: 20,
     },
     inputContainer: {
       height: 70,
       display: "flex",
       alignItems: "center",
-      justifyContent: "space-between"
+      justifyContent: "space-between",
     },
     input: {
       backgroundColor: "white",
@@ -91,7 +89,7 @@ const makeStyles = () => {
       width: 250,
       borderColor: "gray",
       borderWidth: 1,
-      placeholderTextColor: "gray"
+      placeholderTextColor: "gray",
     },
     inputText: {
       backgroundColor: "#143143",
@@ -100,17 +98,17 @@ const makeStyles = () => {
       fontSize: 15 / fontScale,
       borderRadius: 5,
       color: "white",
-      paddingHorizontal: 10
+      paddingHorizontal: 10,
     },
     pageTitle: {
       color: "white",
       fontSize: 40 / fontScale,
-      fontWeight: "bold"
+      fontWeight: "bold",
     },
     progression: {
       color: "white",
       fontSize: 15 / fontScale,
-      alignSelf: "flex-end"
+      alignSelf: "flex-end",
     },
     pageTitleContainer: {
       backgroundColor: "#2D5971",
@@ -123,17 +121,17 @@ const makeStyles = () => {
       display: "flex",
       flexDirection: "row",
       justifyContent: "space-between",
-      padding: 10
+      padding: 10,
     },
     validateButton: {
       backgroundColor: "green",
       paddingHorizontal: 40,
-      borderRadius: 10
+      borderRadius: 10,
     },
     validate: {
       color: "white",
       fontWeight: "bold",
-      fontSize: 25 / fontScale
-    }
+      fontSize: 25 / fontScale,
+    },
   });
 };
