cf6a481c0bf9f476508ecea65b0f8680205a6e7b
chenanef <ychenanef@gmail.com>
2022-12-20 10:22:17 +0100

    no-update

--- screens/ContactsScreen.js
+++ screens/ContactsScreen.js
@@ -3,62 +3,85 @@ import {
   Text,
   StyleSheet,
   Image,
-  useWindowDimensions
+  useWindowDimensions,
+  ImageBackground,
+  TouchableOpacity,
+  Button,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { BACKEND_URL } from "@env"
+import EMail from "../component/EMail";
+import * as Mail from "expo-mail-composer";
+
+
+
+
+
 
 const ContactsScreen = () => {
   const user = useSelector((state) => state.user.value);
   const [mission, setMission] = useState({});
   const styles = makeStyles();
+
+
+ 
+  
+
+
   useEffect(() => {
     (async () => {
       const res = await fetch(
-        `${BACKEND_URL}/missions/${user.mission._id}/${user.userId}`
+        `http://10.2.1.233:3000/missions/${user.mission._id}/${user.userId}`
       );
       const missionData = await res.json();
       setMission(missionData.data);
     })();
   }, []);
+
+
+ 
+  
   return (
     <View style={styles.container}>
-      <Image
+      <ImageBackground
         source={require("../assets/contactBackground.png")}
         style={styles.container}
-      />
-      <View style={styles.pageTitleContainer}>
-        <Text style={styles.pageTitle}>Mes Contacts</Text>
-      </View>
-      <View style={styles.firstContainer}>
-        <View>
+      >
+        <View style={styles.pageTitleContainer}>
+          <Text style={styles.pageTitle}>Mes Contacts</Text>
+        </View>
+
+        <View style={styles.firstContainer}>
           <Text style={styles.textContainer}>Référent Hors Piste</Text>
+
+          {mission.projectReferant && (
+            <Text style={styles.infoContainer}>
+              Name:{mission.projectReferant.surname}{" "}
+              {mission.projectReferant.name}
+              {"\n"}N°:{mission.projectReferant.phoneNumber}
+              {"\n"}
+              Mail:{mission.projectReferant.email}
+            </Text>
+          )}
         </View>
-        {mission.projectReferant && (
-          <Text style={styles.infoContainer}>
-            Name:{mission.projectReferant.surname}{" "}
-            {mission.projectReferant.name}
-            {"\n"}N°:{mission.projectReferant.phoneNumber}
-            {"\n"}
-            Mail:{mission.projectReferant.email}
-          </Text>
-        )}
-      </View>
-      <View style={styles.secondContainer}>
-        <View>
-          <Text style={styles.textContainer}>Référent HAS</Text>
+        <View style={styles.secondContainer}>
+          <View>
+            <Text style={styles.textContainer}>Référent HAS</Text>
+          </View>
+          {mission.projectReferant && (
+            <Text style={styles.infoContainer}>
+              Name:{mission.missionReferant.surname}{" "}
+              {mission.missionReferant.name}
+              {"\n"}N°:{mission.missionReferant.phone}
+              {"\n"}
+              Mail:{mission.missionReferant.email}
+            </Text>
+          )}
         </View>
-        {mission.projectReferant && (
-          <Text style={styles.infoContainer}>
-            Name:{mission.missionReferant.surname}{" "}
-            {mission.missionReferant.name}
-            {"\n"}N°:{mission.missionReferant.phone}
-            {"\n"}
-            Mail:{mission.missionReferant.email}
-          </Text>
-        )}
-      </View>
+        <TouchableOpacity>
+          <Button title=" + Envoyer un mail" color="white" onPress={EMail} />
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
@@ -68,12 +91,12 @@ const makeStyles = () => {
     container: {
       flex: 1,
       width: "100%",
-      height: "100%"
+      height: "100%",
     },
     pageTitle: {
       color: "white",
       fontSize: 40 / fontScale,
-      fontWeight: "bold"
+      fontWeight: "bold",
     },
     pageTitleContainer: {
       backgroundColor: "#2D5971",
@@ -86,21 +109,37 @@ const makeStyles = () => {
       display: "flex",
       flexDirection: "row",
       justifyContent: "space-between",
-      padding: 10
+      padding: 10,
     },
     firstContainer: {
-      backgroundColor: "red"
-    },
-    textContainer: {
-      fontSize: 30
-    },
-    infoContainer: {
-      fontSize: 15
+      backgroundColor: "#a5d8e6",
+      justifyContent: "space-between",
+      marginTop: 250,
+      borderRadius: 15,
+      height: 150,
     },
     secondContainer: {
-      backgroundColor: "violet"
-    }
+      backgroundColor: "#a5d8e6",
+      marginTop: 100,
+      borderRadius: 15,
+      height: 150,
+    },
+
+    textContainer: {
+      fontSize: 30,
+     
+      
+    },
+    infoContainer: {
+      fontSize: 15,
+    },
   });
 };
 
 export default ContactsScreen;
+
+
+
+
+
+
