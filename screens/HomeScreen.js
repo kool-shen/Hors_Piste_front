e3285753ef363f98a9d033b62800ca6ddd8fb770
tagawa <benjamintagawa@gmail.com>
2022-12-22 14:31:51 +0100

    padding

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,6 @@ import {
   TouchableOpacity,
   View,
   useWindowDimensions,
-  KeyboardAvoidingView,
-  ImageBackground,
 } from "react-native";
 import { useSelector } from "react-redux";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
@@ -15,52 +13,53 @@ export default function HomeScreen({ navigation }) {
   const user = useSelector((state) => state.user.value);
   console.log(user);
   return (
-    <KeyboardAvoidingView>
-      <ImageBackground
-        source={require("../assets/backgrounds/homepage.jpg")}
-        style={styles.mainContainer}
-      >
-        <View style={styles.secondContainer}>
-          <TouchableOpacity
-            style={styles.card1}
-            onPress={() => navigation.navigate("MyDocuments")}
-          >
-            <Text style={styles.mainText}>Mes{"\n"}documents</Text>
-            <FontAwesome name="folder-open" size={40} style={styles.icon} />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.card2}
-            onPress={() => navigation.navigate("SignDocuments")}
-          >
-            <FontAwesome name="pencil" size={40} style={styles.icon3} />
-            <Text style={styles.mainText}>Signer</Text>
-            <Text style={styles.secondaryText}> mes documents</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.card3}
-            onPress={() => navigation.navigate("UploadDocuments")}
-          >
-            <Text style={styles.mainText}>Transmettre</Text>
-            <Text style={styles.secondaryText}> mes documents</Text>
-            <FontAwesome name="upload" size={40} style={styles.icon2} />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.card2}
-            onPress={() => navigation.navigate("MyMission")}
-          >
-            <FontAwesome name="plane" size={40} style={styles.icon3} />
-            <Text style={styles.mainText}>Ma mission</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.card1}
-            onPress={() => navigation.navigate("MyContacts")}
-          >
-            <Text style={styles.mainText}>Mes contacts</Text>
-            <FontAwesome name="address-book" size={50} style={styles.icon4} />
-          </TouchableOpacity>
-        </View>
-      </ImageBackground>
-    </KeyboardAvoidingView>
+    <View style={styles.mainContainer}>
+      <View style={styles.container1}>
+        <TouchableOpacity
+          style={styles.card1}
+          onPress={() => navigation.navigate("MyDocuments")}
+        >
+          <Text style={styles.mainText}>Mes{"\n"}documents</Text>
+          <FontAwesome name='folder-open' size={40} style={styles.icon} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container2} >
+        <TouchableOpacity style={styles.card2} onPress={() => navigation.navigate('SignDocuments')}>
+          <FontAwesome name='pencil' size={40} style={styles.icon3}  />
+          <Text style={styles.mainText}>Signer</Text>
+          <Text style={styles.secondaryText}> mes documents</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container3}>
+        <TouchableOpacity
+          style={styles.card3}
+          onPress={() => navigation.navigate("UploadDocuments")}
+        >
+          <Text style={styles.mainText}>Transmettre</Text>
+          <Text style={styles.secondaryText}> mes documents</Text>
+          <FontAwesome name='upload' size={40} style={styles.icon2} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container4}>
+        <TouchableOpacity style={styles.card2} onPress={() => navigation.navigate('MyMission')}>
+          <FontAwesome name='plane' size={40} style={styles.icon3} />
+          <Text style={styles.mainText}>Ma mission</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container5}>
+        <TouchableOpacity
+          style={styles.card1}
+          onPress={() => navigation.navigate("MyContacts")}
+        >
+          <Text style={styles.mainText}>Mes contacts</Text>
+          <FontAwesome
+            name='address-book'
+            size={50}
+            style={styles.icon4}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
 
@@ -77,68 +76,123 @@ const makeStyles = () => {
       fontWeight: "bold",
     },
     mainContainer: {
+      paddingTop: height*0.03,
+      // paddingBottom: height*0.02,
       backgroundColor: "#F8DFBD",
-      height: height,
-      width: width,
-    },
-    secondContainer: {
-      backgroundColor: "transparent",
-      height: height * 0.95,
-      width: width,
-      display: "flex",
-      justifyContent: "space-around",
-      alignItems: "center",
+      height: "100%",
+      width: "100%",
+      flex: 1,
+      zIndex: -1,
     },
     card1: {
       backgroundColor: "#2D5971",
-      width: width * 0.5,
-      height: height * 0.1,
+      transform: [{ rotate: "13deg" }],
+      width: 200,
+      height: 80,
       borderRadius: 10,
       display: "flex",
       flexDirection: "row",
       alignItems: "center",
-      justifyContent: "space-between",
-      padding: "2%",
+      justifyContent: "center",
+      padding: 15,
     },
     card2: {
       backgroundColor: "#2D5971",
-      width: width * 0.5,
-      height: height * 0.1,
+      transform: [{ rotate: "-13deg" }],
+      width: 200,
+      height: 80,
       borderRadius: 10,
       display: "flex",
-      alignItems: "baseline",
+      alignItems: "center",
       justifyContent: "center",
       flexWrap: "wrap",
-      padding: "2%",
+      padding: 15,
     },
     card3: {
       backgroundColor: "#2D5971",
-      width: width * 0.5,
-      height: height * 0.1,
+      transform: [{ rotate: "13deg" }],
+      width: 200,
+      height: 80,
       borderRadius: 10,
       display: "flex",
       justifyContent: "center",
-      alignItems: "center",
+      alignItems: 'center',
       flexWrap: "wrap",
-      padding: "2%",
+      padding: 15,
     },
-
+    container1: {
+      height: height*0.1,
+      backgroundColor: "#F29231",
+      transform: [{ rotate: "-13deg" }, { translateX: -19 }],
+      paddingRight: width*0.07,
+      width: width*1.15,
+      zIndex: 1,
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    container2: {
+      backgroundColor: "#143143",
+      height: height*0.1,
+      transform: [{ rotate: "13deg" }, { translateX: -22 }, { translateY: 13 }],
+      paddingRight: width*0.05,
+      width: width*1.15,
+      zIndex: 3,
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    container3: {
+      backgroundColor: "#F29231",
+      height: "20%",
+      transform: [{ rotate: "-13deg" }, { translateX: -21 }],
+      paddingRight: width*0.07,
+      width: width*1.15,
+      zIndex: 2,
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    container4: {
+      backgroundColor: "#143143",
+      height: "20%",
+      transform: [{ rotate: "13deg" }, { translateX: -10}, {translateY: 15}],
+      paddingRight: width*0.1,
+      width: width*1.15,
+      zIndex: 1,
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    container5: {
+      backgroundColor: "#F29231",
+      height: "20%",
+      transform: [{ rotate: "-13deg" }, { translateX: -21 }],
+      paddingRight: width*0.07,
+      width: width*1.15,
+      zIndex: 0,
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
     icon: {
       color: "white",
-      marginLeft: "20%",
+      marginLeft: 10,
+
     },
     icon2: {
       color: "white",
-      marginLeft: "35%",
+      marginLeft: 10,
     },
     icon3: {
       color: "white",
-      marginRight: "7%",
+      marginRight: 10,
     },
     icon4: {
       color: "white",
-      marginLeft: "5%",
-      marginRight: "5%",
+      marginLeft: 5,
+      marginRight: 5,
+
     },
   });
 };
